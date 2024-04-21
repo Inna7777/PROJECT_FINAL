326a464a6a5563537f6c9a2e101a7dd822dea003
Inna7777 <peshkova-inna@mail.ru>
2024-04-21 21:09:49 +0300

    Created page blog

---
+++ blog_page/script_blog.js
@@ -0,0 +1,16 @@
+
+new Vue ({
+    el:'#app',
+    data:{
+        blogItems:[
+            {image:'../img/blog_Image1.jpg', categoria:'Kitchan Design', titul:'Let’s Get Solution For Building Construction Work', date:'26 December,2022'},
+            {image:`../img/blog_Image2.jpg `, categoria:'Kitchan Design', titul:'Let’s Get Solution For Building Construction Work', date:'26 December,2022'},
+            {image:'../img/blog_Image3.jpg', categoria:'Kitchan Design', titul:'Let’s Get Solution For Building Construction Work', date:'26 December,2022'},
+            {image:'../img/blog_Image1.jpg', categoria:'Kitchan Design', titul:'Let’s Get Solution For Building Construction Work', date:'26 December,2022'},
+            {image:'../img/blog_Image1.jpg', categoria:'Kitchan Design', titul:'Let’s Get Solution For Building Construction Work', date:'26 December,2022'},
+            {image:'../img/blog_Image1.jpg', categoria:'Kitchan Design', titul:'Let’s Get Solution For Building Construction Work', date:'26 December,2022'},
+        ]
+    }
+
+    
+})
